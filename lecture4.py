d4a14fc0482e4733fc420da6aa3dc3e3e60a7212
SergeyKarasyov <sergeyvkarasyov@gmail.com>
2019-01-30 08:06:35 +0100

    lect4

--- lecture4.py
+++ lecture4.py
@@ -1,4 +1,7 @@
 #! /usr/bin/python
+import py_compile
+
+import home2
 
 import hashlib
 m = hashlib.md5()
@@ -21,3 +24,43 @@ def get_random_uuid():
     return hashlib.sha256(l).hexdigest()
 
 print get_random_uuid()
+
+def get_sha(s):
+    import uuid
+    import hashlib
+    import os
+    return hashlib.sha256(s).hexdigest()
+
+print get_sha("")
+
+
+#module -> .py 
+#package -> folder with __init__.py
+
+#module attributes:
+print home2.__name__
+print home2.__doc__
+print home2.__file__
+# print home2.__cached__
+print home2.__package__
+# print home2.__loader__
+
+
+#import sound.effects.echo
+#sound.effects.echo.echofilter(<input>)
+#or
+#from sound.effects import echo
+#echo.echofilter(<input>)
+#or
+#from sound.effects.echo import echofilter
+#echofilter(<input>)
+
+
+#import * from <package> -> if __init__,py takes __all__
+
+# from . import echo
+#from .. import echo
+#from ..Neighbour2ParentDir import something
+
+
+py_compile("lecture4.py")
